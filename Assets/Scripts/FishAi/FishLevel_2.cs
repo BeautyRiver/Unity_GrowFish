1afcee13004c84d65705d6cb72cdc3e33e7aff6a
BeautyRiver <ds5thg@naver.com>
2024-06-08 15:30:13 +0900

    FishLevel2.cs 변경

--- Assets/Scripts/FishAi/FishLevel_2.cs
+++ Assets/Scripts/FishAi/FishLevel_2.cs
@@ -18,33 +18,5 @@ public class FishLevel_2 : FishAi
         base.Update();
     }
 
-    protected override void OnEnable() {
-        base.OnEnable();
-        StartCoroutine(VelocityChange());
-    }
-
-    protected override void OnDisable()
-    {
-        base.OnDisable();
-        StopCoroutine(VelocityChange());
-    }
-    IEnumerator VelocityChange()
-    {
-        while(true)
-        {
-            float tempSpeed = moveSpeed;
-            if (moveSpeed <= 0)
-            {
-                moveSpeed -= 1f * Time.deltaTime;
-            }
-            yield return new WaitForSeconds(Random.Range(2f,4f));
-
-            if (moveSpeed >= tempSpeed)
-            {
-                moveSpeed += 1f * Time.deltaTime;
-            }
-            yield return new WaitForSeconds(Random.Range(3f,5f));
-
-        }
-    }
+    
 }
